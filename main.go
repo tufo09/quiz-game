97acda7629c99efbbd301ea704097afafdc31dd9
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-07-18 14:23:23 +0200

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -39,7 +40,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			question: line[0],
-			answer:   line[1],
+			answer:   strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
